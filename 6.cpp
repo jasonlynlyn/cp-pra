311d5d051b44910d61f18931dc91d0dee12b67a1
jason l <jason.lyn.lol@gmail.com>
2017-11-05 10:27:44 +1100

    cout

---
+++ 6.cpp
@@ -0,0 +1,15 @@
+//cout  === console output 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 28;
+    float average = 12.12;
+    
+    cout << "hi this is printed using cout" << endl << "cout is console output" << endl;
+    cout << "hello world" << endl;
+    cout << "My age is " << age << endl << "the average number is " << average << endl;
+
+    return 0;
+}
