953f620646c135fdb9cdb171924565422bc65e7c
jason l <jason.lyn.lol@gmail.com>
2017-11-05 10:15:42 +1100

    more variables

--- 5.cpp
+++ 5.cpp
@@ -9,14 +9,16 @@ int main()
     int a, b = 10, c;
     int d, e, f; 
     int age = 20;
+    short int cars = 19; //less bytes than int
     char gender = 'm';
     float average;
     bool isit = true;
 
     average = 22.52;
     
-    cout << b << endl;
-    cout << age << endl << gender << endl << average << endl << isit << endl;
+    cout << b << endl; //10
+    cout << age << endl << gender << endl << average << endl << isit << endl << cars << endl;
 
     return 0;
+
 }
