d3cc83e3b013f077e0971a311311c955b2c73e21
jason l <jason.lyn.lol@gmail.com>
2017-11-05 10:10:44 +1100

    more variables

--- 5.cpp
+++ 5.cpp
@@ -6,13 +6,16 @@ using namespace std;
 
 int main()
 {
+    int a, b = 10, c;
+    int d, e, f; 
     int age = 20;
     char gender = 'm';
     float average;
     bool isit = true;
 
     average = 22.52;
-
+    
+    cout << b << endl;
     cout << age << endl << gender << endl << average << endl << isit << endl;
 
     return 0;
