e45e3472af73d4aa5f881715688738a25a99006d
jason l <jason.lyn.lol@gmail.com>
2017-11-05 09:21:26 +1100

    variables

---
+++ 5.cpp
@@ -0,0 +1,19 @@
+//more variables
+//g++ 5.cpp -o 5
+// ./5
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 20;
+    char gender = 'm';
+    float average;
+    bool isit = true;
+
+    average = 22.52;
+
+    cout << age << endl << gender << endl << average << endl << isit << endl;
+
+    return 0;
+}
