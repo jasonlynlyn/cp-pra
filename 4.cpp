1eef3059372c6c927137ef13add1b5c74a0a4554
jason l <jason.lyn.lol@gmail.com>
2017-11-05 09:03:27 +1100

    new

---
+++ 4.cpp
@@ -0,0 +1,16 @@
+//variables
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    char school;
+    float average;
+    bool isit;
+
+    cout << age << endl << school << endl << average << endl << isit;
+
+    return 0;
+
+}
