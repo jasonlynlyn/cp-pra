ed55cd469a4935b8922dd82019ebc70b59b3b22c
jason l <jason.lyn.lol@gmail.com>
2017-11-05 11:12:11 +1100

    operators

---
+++ 8.cpp
@@ -0,0 +1,16 @@
+//arithmetic operators
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 10, num2 = 20;
+
+    cout << num1 << "+" << num2 << "=" << num1+num2 << endl;
+    cout << num1 << "-" << num2 << "=" << num1-num2 << endl;
+    cout << num1 << "*" << num2 << "=" << num1*num2 << endl;
+    cout << num1 << "/" << num2 << "=" << num1/num2 << endl;
+
+    return 0;
+
+}
