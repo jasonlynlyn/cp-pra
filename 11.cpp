5c00a94ce3375348c79c65f0b8bdb095b59bd228
jason l <jason.lyn.lol@gmail.com>
2017-11-05 11:49:47 +1100

    if(condition)

---
+++ 11.cpp
@@ -0,0 +1,20 @@
+//conditional statement if(condition)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 30;
+
+    if( age <=17 ) {
+        
+        cout << "you can't drink" << endl;
+    
+    }else{
+    
+        cout << "you can drink" << endl;
+    }
+
+    return 0;
+
+}
